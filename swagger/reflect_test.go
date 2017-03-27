733f3d8c76b44d7541256e84f93b682d03972ef0
savaki <matt.ho@gmail.com>
2017-03-27 07:36:23 -0700

    - removed dependency on golang 1.7

--- swagger/reflect_test.go
+++ swagger/reflect_test.go
@@ -35,36 +35,34 @@ type ApiResponse struct {
 }
 
 func TestDefine(t *testing.T) {
-	t.Run("Given a thing", func(t *testing.T) {
-		v := define(Pet{})
-		obj, ok := v["swaggerPet"]
-		assert.True(t, ok)
-		assert.False(t, obj.IsArray)
-		assert.Equal(t, 8, len(obj.Properties))
+	v := define(Pet{})
+	obj, ok := v["swaggerPet"]
+	assert.True(t, ok)
+	assert.False(t, obj.IsArray)
+	assert.Equal(t, 8, len(obj.Properties))
 
-		content := map[string]Object{}
-		data, err := ioutil.ReadFile("testdata/pet.json")
-		assert.Nil(t, err)
-		err = json.NewDecoder(bytes.NewReader(data)).Decode(&content)
-		assert.Nil(t, err)
-		expected := content["swaggerPet"]
+	content := map[string]Object{}
+	data, err := ioutil.ReadFile("testdata/pet.json")
+	assert.Nil(t, err)
+	err = json.NewDecoder(bytes.NewReader(data)).Decode(&content)
+	assert.Nil(t, err)
+	expected := content["swaggerPet"]
 
-		assert.Equal(t, expected.IsArray, obj.IsArray, "expected IsArray to match")
-		assert.Equal(t, expected.Type, obj.Type, "expected Type to match")
-		assert.Equal(t, expected.Required, obj.Required, "expected Required to match")
-		assert.Equal(t, len(expected.Properties), len(obj.Properties), "expected same number of properties")
+	assert.Equal(t, expected.IsArray, obj.IsArray, "expected IsArray to match")
+	assert.Equal(t, expected.Type, obj.Type, "expected Type to match")
+	assert.Equal(t, expected.Required, obj.Required, "expected Required to match")
+	assert.Equal(t, len(expected.Properties), len(obj.Properties), "expected same number of properties")
 
-		for k, v := range obj.Properties {
-			e := expected.Properties[k]
-			assert.Equal(t, e.Type, v.Type, "expected %v.Type to match", k)
-			assert.Equal(t, e.Description, v.Description, "expected %v.Required to match", k)
-			assert.Equal(t, e.Enum, v.Enum, "expected %v.Required to match", k)
-			assert.Equal(t, e.Format, v.Format, "expected %v.Required to match", k)
-			assert.Equal(t, e.Ref, v.Ref, "expected %v.Required to match", k)
-			assert.Equal(t, e.Example, v.Example, "expected %v.Required to match", k)
-			assert.Equal(t, e.Items, v.Items, "expected %v.Required to match", k)
-		}
-	})
+	for k, v := range obj.Properties {
+		e := expected.Properties[k]
+		assert.Equal(t, e.Type, v.Type, "expected %v.Type to match", k)
+		assert.Equal(t, e.Description, v.Description, "expected %v.Required to match", k)
+		assert.Equal(t, e.Enum, v.Enum, "expected %v.Required to match", k)
+		assert.Equal(t, e.Format, v.Format, "expected %v.Required to match", k)
+		assert.Equal(t, e.Ref, v.Ref, "expected %v.Required to match", k)
+		assert.Equal(t, e.Example, v.Example, "expected %v.Required to match", k)
+		assert.Equal(t, e.Items, v.Items, "expected %v.Required to match", k)
+	}
 }
 
 func TestHonorJsonIgnore(t *testing.T) {
